2c3e596e0c770ea5490fd7f80842901b0d6826db
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2015-02-09 10:01:24 +0100

    fist commit for dropdown select not working yet

--- source/javascripts/materialo-components/form.js
+++ source/javascripts/materialo-components/form.js
@@ -29,5 +29,81 @@
       }
     });
 
+
+    // Select Plugin
+    $.fn.material_select = function (callback) {
+      $(this).each(function(){
+        $select = $(this);
+        if ( $select.hasClass('browser-default') || $select.hasClass('initialized')) {
+          return; // Continue to next (return false breaks out of entire loop)
+        }
+
+        var uniqueID = guid();
+        var wrapper = $('<div class="select-wrapper"></div>');
+        var options = $('<ul id="select-options-' + uniqueID+'" class="dropdown-content select-dropdown"></ul>');
+        var selectOptions = $select.children('option');
+        if ($select.find('option:selected') !== undefined) {
+          var label = $select.find('option:selected');
+        }
+        else {
+          var label = options.first();
+        }
+
+
+        // Create Dropdown structure
+        selectOptions.each(function () {
+          // Add disabled attr if disabled
+          options.append($('<li class="' + (($(this).is(':disabled')) ? 'disabled' : '') + '"><span>' + $(this).html() + '</span></li>'));
+        });
+
+
+        options.find('li').each(function (i) {
+          var $curr_select = $select;
+          $(this).click(function () {
+            // Check if option element is disabled
+            if (!$(this).hasClass('disabled')) {
+              $curr_select.find('option').eq(i).prop('selected', true);
+              // Trigger onchange() event
+              $curr_select.trigger('change');
+              $curr_select.prev('span.select-dropdown').html($(this).text());
+              if (typeof callback !== 'undefined') callback();
+            }
+          });
+
+        });
+
+        // Wrap Elements
+        $select.wrap(wrapper);
+        // Add Select Display Element
+        var $newSelect = $('<span class="select-dropdown ' + (($select.is(':disabled')) ? 'disabled' : '')
+                         + '" data-activates="select-options-' + uniqueID +'">' + label.html() + '</span>');
+        $select.before($newSelect);
+        $('body').append(options);
+        // Check if section element is disabled
+        if (!$select.is(':disabled')) {
+          $newSelect.dropdown({"hover": false});
+        }
+        $select.addClass('initialized');
+
+      });
+    }
+    // Unique ID for select
+    var guid = (function() {
+      function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+                   .toString(16)
+                   .substring(1);
+      }
+      return function() {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+               s4() + '-' + s4() + s4() + s4();
+      };
+    })();
+
   });
 }( jQuery ));
+
+$(document).ready(function() {
+  $('select').material_select();
+});
+
